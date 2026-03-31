204dfd85ebc0d1d47db37daafa628dfe3daf07cf
BenchGecko <hello@benchgecko.ai>
2026-03-31 23:56:35 +0100

    Add .gitignore, remove build artifacts

--- target/debug/deps/benchgecko-8324e9621dfb5b1c.d
+++
@@ -1,7 +0,0 @@
-/private/tmp/bg-packages/rust/target/debug/deps/benchgecko-8324e9621dfb5b1c.d: src/lib.rs
-
-/private/tmp/bg-packages/rust/target/debug/deps/libbenchgecko-8324e9621dfb5b1c.rlib: src/lib.rs
-
-/private/tmp/bg-packages/rust/target/debug/deps/libbenchgecko-8324e9621dfb5b1c.rmeta: src/lib.rs
-
-src/lib.rs:
